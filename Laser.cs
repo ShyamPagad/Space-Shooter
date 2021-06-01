c8f9b51c432b316a96c65509b23e305b9181d24c
ShyamPagad <shyam3863@gmail.com>
2021-06-01 22:50:31 +0530

    One game object - Capable of shooting a laser
    On pressing spacebar, the gameobject can shoot laser (or any other game object can be implemented).

---
+++ Laser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private float _speed = 8.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up*_speed*Time.deltaTime);
+        //if laser pos is greater than 8 on y axis
+        //destroy the object
+        if(transform.position.y > 8)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
